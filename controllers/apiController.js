19f5bd80110f1449db5ec3610b313c76f7ff1d71
juandaza9919 <Juandaza.9919@gmail.com>
2023-11-13 21:07:50 -0500

    finalcommitdomain

---
+++ controllers/apiController.js
@@ -0,0 +1,39 @@
+const {Series} = require('../models/series')
+
+
+class ApiController  { 
+    
+    testApi   (req, res)  {
+        res.send('Hello world!')
+    }
+    async getApi   (req, res)  {
+        const list = await Series.find();
+        console.log(list)
+        res.status(200).json(list)
+    }
+    async postApi  (req, res)  {
+        try {
+            const newSerie = new Series(req.body)
+            await newSerie.save()
+            console.log(newSerie)
+            res.status(201).json(newSerie)
+        } catch (error) {
+            console.log(error)
+            res.status(400).json(error)
+        }
+    }
+    
+    putApi  (req, res)  {
+        res.status(201).send('Esto es una api put')
+    }
+    patchApi  (req, res)  {
+        res.status(404).send('Esto es una api patch')
+        }
+    deleteApi  (req, res)  {
+        res.status(200).send('Esto es una api delete')
+        }
+}
+
+
+
+module.exports = new ApiController
---
+++ routers/user.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+const userController = require('../controllers/userController')
+const auth = require('../milddlewares/auth')
+const checks = require('../milddlewares/loginChecks')
+const {validarChecks} = require('../milddlewares/validarChecks')
+const validarToken = require('../milddlewares/validarToke')
+
+
+router.get('/session', userController.pruebaSession)
+router.get('/test', auth, userController.test)
+router.get('/borrar', userController.borrarSession)
+router.post('/login', checks, validarChecks, userController.login)   
+router.delete('/logout', userController.logout)
+
+
+router.post('/generador', userController.generador)
+router.get('/validar', validarToken, userController.pasoElToken)
+router.post('/loginjwt', userController.loginJWT)
+
+module.exports = router
